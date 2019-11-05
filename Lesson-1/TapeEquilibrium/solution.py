5c85ed361ed1a3d6ab261a6d2008f749717e8a40
Harivardhan Pyaram <p.hari.vrdhn@gmail.com>
2019-11-04 23:31:04 -0800

    Update solution.py
    Changing to Python 3.6...

--- Lesson-1/TapeEquilibrium/solution.py
+++ Lesson-1/TapeEquilibrium/solution.py
@@ -1,16 +1,22 @@
 def solution(A):
-    # write your code in Python 2.7
+    # write your code in Python 3.6
+    # calculate sum
     sum = 0
-    for p in A:
-        sum = sum + p
+    for x in A:
+        sum += x
     
+    # set q to 0
     q = 0
-    for p in range(0,len(A)):
-       q = q + A[p]
-       diff = abs((sum - q) - q)
-       if ( p == 0 ) :
-           min = diff
-       elif ( diff < min ):
-           min = diff
-    
+    for i in range(0, len(A)):
+        # increment q with next value (move split to next step)
+        q += A[i]
+        
+        # calculate absolute diff between the two parts
+        diff = abs(q - (sum - q))
+        
+        # if its the first time set min to start value
+        # else check if min is lesser
+        if i == 0 or diff < min:
+            min = diff
     return min
+    pass
