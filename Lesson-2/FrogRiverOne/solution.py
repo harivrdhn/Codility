b3257a1ea6650975192ea09d19ac27bdecdbe029
Harivardhan Pyaram <p.hari.vrdhn@gmail.com>
2019-11-05 00:58:12 -0800

    Update solution.py
    Updating to new Python and add comments, optimize code..

--- Lesson-2/FrogRiverOne/solution.py
+++ Lesson-2/FrogRiverOne/solution.py
@@ -1,10 +1,21 @@
 def solution(X, A):
-    R = [False]* (X+1)
-    count = 0
-    for i in range (0,len(A)):
-        if not R[A[i]]:
-            count += 1
-            R[A[i]] = True
-        if count == X:
-            return i
+    # write your code in Python 3.6
+    
+    # if river is 0, return.
+    if X == 0:
+        return 0
+    # if number of keaves less than river, return
+    elif  len(A) < X:
+        return -1
+    
+    # array of places
+    places = [False] * (X + 1)
+    i = 0
+    for k in range(0, len(A)):
+        if not places[A[k]]: # if no leaf in place
+            places[A[k]] = True
+            i += 1 # increment count
+            if i == X: # if count and places are equal, we have leaf in each place
+                return k
     return -1
+    pass
